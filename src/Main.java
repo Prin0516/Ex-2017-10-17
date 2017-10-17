60967cd6f54ef705401687b9b485daac6561bf49
Prin0516 <105021046@live.asia.edu.tw>
2017-10-17 11:49:40 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        MainFrame mf=new MainFrame();
+        LoginFrame mf=new LoginFrame();
         mf.setVisible(true);
     }
 }
